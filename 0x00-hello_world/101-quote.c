81d4319cfad5e39fc53672d61c3683338761ac7b
Hilda-254 <hildamunjuri@gmail.com>
2022-12-10 05:05:00 -0800

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Prints out the last part of a quote in the standard error
- * Return: 1 if success.
+ * main - Entry point
+ *
+ * Description: prints a string to the standard error
+ *
+ * Return: Always 1(Error)
  */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n",58);
-	return (1);
+char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n";
+
+write(2,  err, 59);
+
+return (1);
+
 }
