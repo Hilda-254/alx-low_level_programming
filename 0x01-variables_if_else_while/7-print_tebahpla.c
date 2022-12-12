c04b87f9ae6f7ce9bf72120e4b2c0f46d0846d80
Hilda-254 <hildamunjuri@gmail.com>
2022-12-12 07:18:47 -0800

    Tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,9 +2,9 @@
 #include <time.h>
 #include <stdlib.h>
 /**
- * main -main block
+ * main - main block
  * Description: prints all single digit number of base 10
- * starting from 0, followed by a new line 
+ * Starting from 0, followed by a new line
  * Return: 0
  */
 int main(void)
