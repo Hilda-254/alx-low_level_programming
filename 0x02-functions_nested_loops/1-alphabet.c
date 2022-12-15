65c2c122af2a369e749e8e9887e6b5b4e75c61b9
Hilda-254 <hildamunjuri@gmail.com>
2022-12-14 20:27:53 -0800

    Lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * Entry point - print lowercase
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
+
+
